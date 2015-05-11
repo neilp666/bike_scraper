dfa4cf13b8bbfad5630d42f4c3b0dad067e44c15
neil patel <neil@myindianweddingapp.com>
2015-05-11 11:49:46 +0100

    Added advanced search filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all.paginate(:page => params[:page], :per_page => 30)
+    @posts = @posts.where(state: params[:state]) if params["state"].present?
+    @posts = @posts.where(price: params[:price]) if params["price"].present?
   end
 
   # GET /posts/1
